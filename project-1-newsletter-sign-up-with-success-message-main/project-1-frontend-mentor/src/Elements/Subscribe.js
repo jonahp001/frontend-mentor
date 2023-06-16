7ae0b42a3c36a4344066ef0d75d7932fbea637f9
Jonah Park <jonahp1234@gmail.com>
2023-06-16 04:17:34 +0000

    Got most of element, need to implement the form section

---
+++ project-1-newsletter-sign-up-with-success-message-main/project-1-frontend-mentor/src/Elements/Subscribe.js
@@ -0,0 +1,68 @@
+export default function Subscribe() {
+  return (
+    <div className="subscribe col">
+      <header>
+        <h1>Stay Updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+      </header>
+      <div>
+        <div className="mb-1">
+          <svg
+            className="me-3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21">
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path
+                stroke="#FFF"
+                stroke-width="2"
+                d="M6 11.381 8.735 14 15 8"
+              />
+            </g>
+          </svg>
+          <p className="d-inline">
+            Product discovery and building what matters
+          </p>
+        </div>
+        <div className="mb-1">
+          <svg
+            className="me-3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21">
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path
+                stroke="#FFF"
+                stroke-width="2"
+                d="M6 11.381 8.735 14 15 8"
+              />
+            </g>
+          </svg>
+          <p className="d-inline">Measuring to ensure updates are a success</p>
+        </div>
+        <div className="mb-1">
+          <svg
+            className="me-3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="21"
+            height="21"
+            viewBox="0 0 21 21">
+            <g fill="none">
+              <circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
+              <path
+                stroke="#FFF"
+                stroke-width="2"
+                d="M6 11.381 8.735 14 15 8"
+              />
+            </g>
+          </svg>
+          <p className="d-inline">And much more!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
