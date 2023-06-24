53f91fef56bad52a5ea02dc4d10eeac12b182ca8
Jonah Park <jonahp1234@gmail.com>
2023-06-24 07:00:33 +0000

    redo of javascript-ajax complete

---
+++ practice-lfz/javascript-ajax/users.js
@@ -0,0 +1,17 @@
+const userList = document.querySelector('#user-list');
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+xhr.responseType = 'json';
+
+xhr.addEventListener('load', () => {
+  console.log(xhr.status);
+  console.log(xhr.response);
+  for (let i = 0; i < xhr.response.length; i++) {
+    const newLi = document.createElement('li');
+    newLi.textContent = xhr.response[i].name;
+    userList.appendChild(newLi);
+  }
+});
+
+xhr.send();
