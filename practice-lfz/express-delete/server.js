f759a1dbcc92c6e22ccc5e56a2eb00fc15f74b83
Jonah Park <jonahp1234@gmail.com>
2023-09-20 19:21:15 +0000

    express delete review

---
+++ practice-lfz/express-delete/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradeList = [];
+  for (const grade in grades) {
+    gradeList.push(grades[grade]);
+  }
+  res.json(gradeList);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  console.log(req.params.id);
+  delete grades[req.params.id];
+  res.sendStatus(204);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000!');
+});
