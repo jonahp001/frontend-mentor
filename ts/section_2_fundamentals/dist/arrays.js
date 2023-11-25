36d919405d0c50881ef7e8a552b1a07d9034f242
Jonah Park <jonahp1234@gmail.com>
2023-11-25 19:31:52 +0000

    TS tutorial 2

---
+++ ts/section_2_fundamentals/dist/arrays.js
@@ -0,0 +1,4 @@
+"use strict";
+let numbers = [];
+numbers.forEach(n => n.toFixed);
+//# sourceMappingURL=arrays.js.map
---
+++ ts/section_2_fundamentals/src/enums.ts
@@ -0,0 +1,12 @@
+// const small = 1;
+// const med = 2;
+// const large = 3;
+
+// PascalCase
+const enum Size {
+  Small = 1,
+  Med,
+  Large
+};
+let mySize: Size = Size.Med;
+console.log(mySize)
